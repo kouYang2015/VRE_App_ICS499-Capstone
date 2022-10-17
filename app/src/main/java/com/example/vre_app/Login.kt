f262693d8eb048a4e4ea04f3ef001252ed07e70d
jleejl <jacklee0284@gmail.com>
2022-10-16 21:26:11 -0500

    Created Login activity file

---
+++ app/src/main/java/com/example/vre_app/Login.kt
@@ -0,0 +1,11 @@
+package com.example.vre_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
