2aae709564901eb604ac842dfbf3a1138bd7cb8a
jleejl <jacklee0284@gmail.com>
2022-10-16 21:28:07 -0500

    Add lateinit var

--- app/src/main/java/com/example/vre_app/Login.kt
+++ app/src/main/java/com/example/vre_app/Login.kt
@@ -2,8 +2,17 @@ package com.example.vre_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
+    private lateinit var loginButton: Button
+    private lateinit var createAccountButton: Button
+    private lateinit var forgotPasswordTextClickable: TextView
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
