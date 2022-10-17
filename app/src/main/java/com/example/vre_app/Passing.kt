08bc29a41eaeefdb93301922cebef2bf789d35c8
Joshua Vang <vangjosh95@gmail.com>
2022-10-17 11:17:13 -0500

    Forgot to add a complete statement

--- app/src/main/java/com/example/vre_app/Passing.kt
+++ app/src/main/java/com/example/vre_app/Passing.kt
@@ -1,8 +1,8 @@
 package com.example.vre_app
 //Tried to save username and password (First Time login-in)
 object Passing {
-     var username: String = ""
-     var password: String = ""
+    var username: String = ""
+    var password: String = ""
 
     @JvmName("setUsername1")
     fun setUsername(username: String) {
@@ -13,4 +13,5 @@ object Passing {
     fun setPassword(password: String) {
         password.also { Passing.password = it }
 
+    }
 }
