62b960ecd2cda3217c4f9304ec68093ccc93e480
jleejl <jacklee0284@gmail.com>
2022-10-17 10:26:23 -0500

    Add code for button

--- app/src/main/java/com/example/vre_app/Login.kt
+++ app/src/main/java/com/example/vre_app/Login.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
     private lateinit var loginButton: Button
@@ -15,6 +16,56 @@ class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This is to hide the action bar
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+
+        loginButton = findViewById(R.id.login_button)
+        createAccountButton = findViewById(R.id.create_account_button)
+        forgotPasswordTextClickable = findViewById(R.id.forgot_password)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+
+        loginButton.setOnClickListener {
+            authenticateLogin()
+        }
+
+        createAccountButton.setOnClickListener {
+            val text = "Create an Account is clickable"
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(this, text, duration)
+
+            toast.show()
+        }
+
+        forgotPasswordTextClickable.setOnClickListener {
+            val text = "Forgot your password? is clickable"
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(this, text, duration)
+
+            toast.show()
+        }
+
+    }
+
+    // This is the login for username and password
+    private fun authenticateLogin() {
+        val inputUsername = username.text.toString()
+        val inputPassword = password.text.toString()
+
+        if (inputUsername == "username" && inputPassword == "password") {
+            val text = "Thank you, you are logged in."
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(this, text, duration)
+            toast.show()
+        }
+        else {
+            val text = "The Username or Password is incorrect, Try Again."
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(this, text, duration)
+            toast.show()
+        }
+
     }
 }
