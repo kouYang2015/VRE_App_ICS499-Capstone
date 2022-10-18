fd12a2d7b1e8ee0a1eab231718a94edb0eba1cde
Kou <kou.yang2015@gmail.com>
2022-10-17 21:34:12 -0500

    Add logic for MainActivity to navigate to ListenSpeechActivity

--- app/src/main/java/com/example/vre_app/MainActivity.kt
+++ app/src/main/java/com/example/vre_app/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.vre_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button = findViewById<Button>(R.id.openSpeechListener)
+
+        // operations to be performed when user tap on the button
+        button?.setOnClickListener() {
+            val intent = Intent(this, ListenSpeechActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-   // Changed the comment
-   // seth branch commit and push test
-    // Make a change to seth's branch
 }
