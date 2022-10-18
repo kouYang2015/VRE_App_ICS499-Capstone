fb234c6e5eb1ce284033165d8db62615f22afa69
Kou <kou.yang2015@gmail.com>
2022-10-18 02:29:43 -0500

    Cleaned up MainActivity, make button navigate to ListenSpeechActivity

--- app/src/main/java/com/example/vre_app/MainActivity.kt
+++ app/src/main/java/com/example/vre_app/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
