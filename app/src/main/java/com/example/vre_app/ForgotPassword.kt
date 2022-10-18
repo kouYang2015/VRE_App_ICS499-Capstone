60e943b699aa117806b98ecaa0717b29c9026e0c
jleejl <97915116+jleejl@users.noreply.github.com>
2022-10-17 22:22:42 -0500

    Authenticate login (#7)
    * Created Login activity file
    
    * Add lateinit var
    
    * Add text, button, and images to UI
    
    * Add hide action bar
    
    * Add code for button
    
    * Change AndroidManifest to launch .Login first
    
    * Change login UI and add Menu activity
    
    * Add Menu UI, login onclick goes to menu if correct username and password
    
    * Change background color for Login UI and Menu UI.
    
    * Add LabelFor for username and password to fix error
    
    * Add ForgotPassword activity and edited the Forgot Your Password? UI screen
    
    * Change Login user input for password to show "*"
    
    * Delete the string name or_text
    
    * Delete the TextView for the word "or" from activity_login.xml

---
+++ app/src/main/java/com/example/vre_app/ForgotPassword.kt
@@ -0,0 +1,15 @@
+package com.example.vre_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // This is to hide the action bar
+        supportActionBar?.hide()
+        
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
