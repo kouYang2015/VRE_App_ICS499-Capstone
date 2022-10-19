3d1d97c72e57efbd73c1749b1f837f8f64ff982a
seastwood <eastwoodseth@gmail.com>
2022-10-18 19:43:36 -0500

    Merged Master into branch

--- app/src/main/java/com/example/vre_app/MainActivity.kt
+++ app/src/main/java/com/example/vre_app/MainActivity.kt
@@ -2,11 +2,8 @@ package com.example.vre_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.*
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 
 
 class MainActivity : AppCompatActivity() {
