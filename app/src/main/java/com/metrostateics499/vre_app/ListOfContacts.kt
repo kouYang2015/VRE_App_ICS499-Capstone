67244e79d15aa96b93cf4ee2a3f397f2d828b7f3
Joshua Vang <vangjosh95@gmail.com>
2022-11-04 19:30:49 -0500

    Display the contact Name and Phone Number

--- app/src/main/java/com/metrostateics499/vre_app/ListOfContacts.kt
+++ app/src/main/java/com/metrostateics499/vre_app/ListOfContacts.kt
@@ -2,6 +2,7 @@ package com.metrostateics499.vre_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_list_of_contacts.*
 
 class ListOfContacts : AppCompatActivity() {
 
@@ -9,6 +10,9 @@ class ListOfContacts : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_of_contacts)
 
+        var contact = intent.getSerializableExtra("key") as Contacts
+        textName.text = contact.names
+        textPhone.text = contact.phone
 
     }
 }
