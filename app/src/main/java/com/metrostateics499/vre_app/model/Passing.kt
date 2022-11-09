85c122b03de6cf2406e3c481dbf28931556b130e
Kou Yang <80478758+kouYang2015@users.noreply.github.com>
2022-11-09 16:46:26 -0600

    Remove unnecessary functions and variables. (#30)

--- app/src/main/java/com/metrostateics499/vre_app/model/Passing.kt
+++ app/src/main/java/com/metrostateics499/vre_app/model/Passing.kt
@@ -1,28 +1,22 @@
 package com.metrostateics499.vre_app.model
 
 import com.metrostateics499.vre_app.model.data.*
-import java.util.*
 
-// Tried to save username and password (First Time login-in)
+/**
+ * Singleton object that is used to store user data.
+ *
+ * @constructor Create empty Passing singleton object.
+ */
 object Passing {
     var username: String = ""
     var password: String = ""
     var email: String = ""
-
-    private var newKeyPhraseList: MutableList<KeyPhrase> = LinkedList()
-    var keyPhraseList = KeyPhraseList(newKeyPhraseList)
-
-    private var newCustomTextMessageListList: MutableList<CustomTextMessage> = LinkedList()
-    var customTextMessageList = CustomTextMessageList(newCustomTextMessageListList)
-
-    private var newEmergencyMessageSetupListList: MutableList<EmergencyMessageSetup> = LinkedList()
-    var emergencyMessageSetupList = EmergencyMessageSetupList(newEmergencyMessageSetupListList)
-
+    var keyPhraseList = KeyPhraseList(mutableListOf())
+    var customTextMessageList = CustomTextMessageList(mutableListOf())
+    var emergencyMessageSetupList = EmergencyMessageSetupList(mutableListOf())
+    var contactList = ContactList(mutableListOf())
     var selectedEmergencyMessageSetup: EmergencyMessageSetup? = null
     var selectedContact: Contact? = null
-
-    private var newContactList: MutableList<Contact> = LinkedList()
-    var contactList = ContactList(newContactList)
 
     @JvmName("setUsername1")
     fun setUsername(username: String) {
@@ -37,15 +31,5 @@ object Passing {
     @JvmName("setPassword1")
     fun setPassword(password: String) {
         password.also { Passing.password = it }
-    }
-
-    /**
-     * Add key phrase
-     *
-     * @param keyPhrase
-     */
-    @JvmName("createKeyPhraseList")
-    fun addKeyPhrase(keyPhrase: KeyPhrase) {
-        keyPhraseList.addKeyPhrase(keyPhrase)
     }
 }
