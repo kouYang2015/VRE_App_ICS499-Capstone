9a92e9090347eed4cb690f62308f5d702c111845
jleejl <jacklee0284@gmail.com>
2022-12-03 06:53:55 -0600

    Started a new branch under UpdateForgotPassword Add the new activity and xml for Forgot Email Username

--- app/src/main/java/com/metrostateics499/vre_app/view/ForgotEmailUsernameActivity.kt
+++ app/src/main/java/com/metrostateics499/vre_app/view/ForgotEmailUsernameActivity.kt
@@ -1,7 +1,7 @@
 package com.metrostateics499.vre_app.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.metrostateics499.vre_app.R
 
 class ForgotEmailUsernameActivity : AppCompatActivity() {
