8de088ddf629907e791ab3a3a5511d31fdf97137
jleejl <jacklee0284@gmail.com>
2022-12-03 08:19:04 -0600

    Wrote code for ForgotEmailUsername Activity. Change android id names for activity_forgot_email_username XML. Reword some text in strings XML Add VerificationCodeLogin Activity.kt

--- app/src/main/java/com/metrostateics499/vre_app/view/ForgotEmailUsernameActivity.kt
+++ app/src/main/java/com/metrostateics499/vre_app/view/ForgotEmailUsernameActivity.kt
@@ -1,12 +1,89 @@
 package com.metrostateics499.vre_app.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.metrostateics499.vre_app.R
+import com.metrostateics499.vre_app.model.Passing
 
 class ForgotEmailUsernameActivity : AppCompatActivity() {
+
+    private lateinit var verifyEmailUsername: EditText
+    private lateinit var verifyEmailUsernameButton: Button
+    private lateinit var invalidEmailUsername: TextView
+    private var adminUsername: String = "username"
+    private var generatedNumber: String = ""
+
+    /**
+     *  This overrides the onCreate of this activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // This is to hide the action bar
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_forgot_email_username)
+
+        verifyEmailUsername = findViewById(R.id.forgot_emailUsername_verify_email_username)
+        verifyEmailUsernameButton =
+            findViewById(R.id.forgot_emailUsername_verify_email_username_button)
+        invalidEmailUsername = findViewById(R.id.forgot_emailUsername_invalid)
+
+        // This is the verify button
+        verifyEmailUsernameButton.setOnClickListener {
+            verifyAccount()
+        }
     }
+
+    /**
+     * Verifying if username is in the system
+     */
+    private fun verifyAccount() {
+        val inputEmailUsername = verifyEmailUsername.text.toString()
+        val emptyEmailUsernameInfo = "Email or Username can not be empty"
+        val notFoundEmailUsername = "Sorry, Email or Username was not found"
+
+        // Check and display message if Email/Username is empty, is correct, and is incorrect
+        if (verifyEmailUsername.text.isEmpty()) {
+            // Display message telling user the name can not be empty
+            invalidEmailUsername.text = emptyEmailUsernameInfo
+        } else if ((
+                    (inputEmailUsername == Passing.username || inputEmailUsername == Passing.email) ||
+                            (inputEmailUsername == adminUsername)
+                    )
+        ) {
+            verificationRandomCode()
+
+            // Display message when successfully verified
+            Toast.makeText(this, "Account Found", Toast.LENGTH_LONG)
+                .show()
+            val intent = Intent(this, VerificationCodeActivity::class.java)
+                .putExtra("genNum", generatedNumber)
+                .putExtra("email/username", inputEmailUsername)
+            startActivity(intent)
+
+            // Clears on screen message
+            invalidEmailUsername.text = ""
+            // Clears verify username on the screen
+            verifyEmailUsername.text.clear()
+        } else {
+            // Display message telling user username is not found
+            invalidEmailUsername.text = notFoundEmailUsername
+        }
+    }
+
+    /**
+     * Generate a random 6 number total for verification
+     */
+    private fun verificationRandomCode() {
+        val randomNumber = (123457..987654).shuffled().last()
+
+        generatedNumber = randomNumber.toString()
+    }
+
 }
