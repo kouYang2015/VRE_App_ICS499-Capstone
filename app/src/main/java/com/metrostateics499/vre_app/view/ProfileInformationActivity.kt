fd614f94377274c03a4fdb1795fa4057b0510810
jleejl <jacklee0284@gmail.com>
2022-12-06 20:58:16 -0600

    Add profile information activity and xml. Fix Error with Unresolved ref R in profile information activity.

---
+++ app/src/main/java/com/metrostateics499/vre_app/view/ProfileInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.metrostateics499.vre_app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metrostateics499.vre_app.R
+
+class ProfileInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_information)
+    }
+}
